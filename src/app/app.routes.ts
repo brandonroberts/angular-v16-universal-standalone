84a4e8ba236d5d391baed615f433ed39dbd2cc4e
Brandon Roberts <robertsbt@gmail.com>
2023-04-06 16:33:47 -0500

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', loadComponent: () => import('./home/home.component')
+  },
+  {
+    path: 'about', loadComponent: () => import('./about/about.component')
+  },
+  {
+    path: 'todos', loadComponent: () => import('./todos/todos.component')
+  }  
+];
